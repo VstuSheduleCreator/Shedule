ad72350e20d705e358f84c3254ab4a79687ca3d1
Alexey Berezhnoy <bobrokot1488@gmail.com>
2016-10-30 20:58:40 +0300

    Изменение SheduleDay и TestSheduleDay
    Логика из методов вызываемых в конструкторе перенесена в конструктор. Добавлены новые тесты для конструкторов.

--- MyShedule/SheduleClasses/SheduleDay.cs
+++ MyShedule/SheduleClasses/SheduleDay.cs
@@ -29,14 +29,25 @@ namespace MyShedule
 
         public SheduleDay(Week week, Day day, IEnumerable<SheduleRoom> rooms, SettingShedule setting, DateTime firstDate)
         {
-            //times
             Week = week;
             Day = day;
-            //settings
-            InitializeSettingOfDay(setting, day);
-            //main
-            InitializeDatesOfDay(firstDate, setting.CountWeeksShedule, setting.CountEducationalWeekBySem);
-            Lessons = InitializeLessonsOfDay(rooms.ToList(), setting.CountLessonsOfDay).ToList();
+
+            // инициализировать настройки
+            // с понедельника по пятницу и в выходные часы для проставления пар отличаются
+            _earlierPossibleHour = (int)day <= (int)Day.Friday ? setting.FirstLessonsOfWeekDay : setting.FirstLessonsOfWeekEnd;
+            _lastPossibleHour = (int)day <= (int)Day.Friday ? setting.LastLessonsOfWeekDay : setting.LastLessonsOfWeekEnd;
+            _maxPossibleCountLessons = (int)day <= (int)Day.Friday ? setting.MaxCountLessonsOfWeekDay : setting.MaxCountLessonsOfWeekEnd;
+
+            // инициализ. список календарных дат, добавляем первую дату, остальные вычисляем на основе первой даты
+            Dates = new List<DateTime>() { firstDate };
+                for(int date = 1; date < (int)Math.Ceiling((decimal)setting.CountEducationalWeekBySem / setting.CountWeeksShedule); date++)
+                    Dates.Add(Dates.Last() + TimeSpan.FromDays(countDaysByWeek * setting.CountWeeksShedule));
+
+            // создать занятия для определенного дня
+            Lessons = new List<SheduleLesson>();
+            foreach(SheduleRoom room in rooms)
+                for(int counterHour = EarlierPossibleHour; counterHour <= LastPossibleHour && counterHour <= MaxPossibleCountLessons; counterHour++)
+                    Lessons.Add(new SheduleLesson(new SheduleTime(Week, Day, counterHour), room.Name, Dates));
         }
 
         #endregion
@@ -69,45 +80,6 @@ namespace MyShedule
 
         #endregion
 
-        //создать занятия для определенного дня, метод вызывается в конструкторе
-        private IEnumerable<SheduleLesson> InitializeLessonsOfDay(IEnumerable<SheduleRoom> rooms, int countHoursCreating)
-        {
-            //у каждого дня в расписании имеется двумерная сетка занятий, на одной оси сетки
-            //находятся аудитории всего N,  на другой номера пар всего M, т.е. M на N занятий
-            foreach (SheduleRoom room in rooms)
-                for (int counterHour = 1; counterHour <= countHoursCreating; counterHour++)
-                    yield return new SheduleLesson(new SheduleTime(Week, Day, counterHour), room.Name, GetDatesLesson());
-        }
-    
-        //инициализировать настройки
-        private void InitializeSettingOfDay(SettingShedule setting, Day day)
-        {
-            //с понедельника по пятницу и в выходные часы для проставления пар отличаются
-            _earlierPossibleHour = (int) day <= (int) Day.Friday ? setting.FirstLessonsOfWeekDay : setting.FirstLessonsOfWeekEnd;
-            _lastPossibleHour = (int) day <= (int) Day.Friday ? setting.LastLessonsOfWeekDay : setting.LastLessonsOfWeekEnd;
-            _maxPossibleCountLessons = (int) day <= (int) Day.Friday ? setting.MaxCountLessonsOfWeekDay : setting.MaxCountLessonsOfWeekEnd;
-        }
-
-        /// <summary> обновить настройки у дня </summary>
-        public void UpdateSetting(SettingShedule setting){
-            InitializeSettingOfDay(setting, Day);
-        }
-
-        //расставить календарные даты дня
-        private void InitializeDatesOfDay(DateTime firstDate, int countWeekShedule, int countWeeksSem)
-        {
-            //инициализ. список календарных дат, добавляем первую дату, остальные вычисляем на основе первой даты
-            Dates = new List<DateTime>() { firstDate };
-            for (int date = 1; date <= countWeeksSem / countWeekShedule; date++)
-                Dates.Add(Dates.Last() + TimeSpan.FromDays(countDaysByWeek * countWeekShedule));
-        }
-
-        //получить даты занятия в этот день
-        private IEnumerable<DateTime> GetDatesLesson() {
-            for (int i = 0; i < Dates.Count; i += 2)
-              yield return Dates[i];
-        }
-
         /// <summary> Первая проставленная пара в дне </summary>
         public int FirstLessonHour { get { return CountLessons > 0 ? NonEmptyLessons.First().Hour : 0; } }
 
@@ -120,7 +92,7 @@ namespace MyShedule
 
         /// <summary> получить количество занятий у определенной группы в этот день </summary>
         public int CountLessonsGroup(string nameGroup, Week week, Day day) {
-            return (from x in NonEmptyLessons from grp in x.Groups where !x.IsEmpty && grp == nameGroup && x.Week == week && x.Day == day select x).Count();
+            return (from x in NonEmptyLessons from grp in x.Groups where grp == nameGroup && x.Week == week && x.Day == day select x).Count();
         }
 
         /// <summary> проверить не будет переполнен день если добавить одно занятие </summary>
@@ -152,15 +124,14 @@ namespace MyShedule
         /// <summary> строка со списком календарных дат дня в формате {0:dd}.{0:MM} </summary>
         public string DatesDescription
         {
-            get {
+            get
+            {
                 string datesLine = String.Empty;
                 foreach (DateTime dt in Dates)
                 {
                     datesLine += String.Format("{0:dd}.{0:MM}", dt);
-                    // TODO: скорее здесль лучше использовать обычный if 
                     datesLine += Dates.Last() != dt ? ", " : "";
                 }
-
                 return datesLine;
             }
         }
--- TestShedule/TestSheduleDay.cs
+++ TestShedule/TestSheduleDay.cs
@@ -9,13 +9,13 @@ namespace TestShedule
     [TestClass]
     public class TestSheduleDay
     {
-        private SheduleDay day = new SheduleDay();
-        private SheduleDay weekday;
+        private SheduleDay day;
 
-        // TODO: написать тесты для SheduleRoom
-        private SheduleDay getWeekDay()
+        private SheduleDay getDay(SettingShedule setting = null, Week week = Week.FirstWeek, Day dayWeek = Day.Monday)
         {
-            SettingShedule setting = new SettingShedule(2, 6, 6, 6, 4, 4, 2, 1, 1, 4, 2);
+            if(setting == null)
+                setting = new SettingShedule(2, 6, 6, 6, 16, 4, 2, 1, 2, 4, 3);
+
             IEnumerable<SheduleRoom> rooms;
 
             dsShedule sheduleDataSet = new dsShedule();
@@ -23,31 +23,81 @@ namespace TestShedule
             sheduleDataSet.Room.ReadXml(filename);
             rooms = DictionaryConverter.RoomsToList(sheduleDataSet);
 
-            return new SheduleDay(Week.FirstWeek, Day.Monday, rooms, setting, new DateTime(2016, 9, 1));
+            return new SheduleDay(week, dayWeek, rooms, setting, new DateTime(2016, 9, 1));
         }
 
         [TestMethod]
-        public void TestUpdateSettingOfWeekDay()
+        public void TestEmptyConstrucor()
         {
-            day = getWeekDay();
-            SettingShedule setting = new SettingShedule(2, 6, 6, 6, 4, 6, 4, 1, 1, 6, 4);
-            day.UpdateSetting(setting);
+            day = new SheduleDay();
 
-            Assert.AreEqual(day.MaxPossibleCountLessons, 6);
-            Assert.AreEqual(day.LastPossibleHour, 6);
-            Assert.AreEqual(day.EarlierPossibleHour, 1);
+            Assert.AreEqual(Week.Another, day.Week);
+            Assert.AreEqual(Day.Another, day.Day);
+
+            Assert.AreEqual(0, day.Dates.Count);
+            Assert.AreEqual(0, day.Lessons.Count);
+
+            Assert.AreEqual(0, day.EarlierPossibleHour);
+            Assert.AreEqual(0, day.LastPossibleHour);
+            Assert.AreEqual(0, day.MaxPossibleCountLessons);
+        }
+
+        [TestMethod]
+        public void TestConstructorForWeekDay()
+        {
+            day = getDay(dayWeek: Day.Monday);
+
+            Assert.AreEqual(Week.FirstWeek, day.Week);
+            Assert.AreEqual(Day.Monday, day.Day);
+            Assert.AreEqual(8, day.Dates.Count);
+            Assert.AreEqual(32, day.Lessons.Count);
+
+            Assert.AreEqual(1, day.EarlierPossibleHour);
+            Assert.AreEqual(4, day.LastPossibleHour);
+            Assert.AreEqual(4, day.MaxPossibleCountLessons);
+        }
+
+        [TestMethod]
+        public void TestConstructorForWeekEnd()
+        {
+            day = getDay(dayWeek: Day.Saturday);
+            Assert.AreEqual(Week.FirstWeek, day.Week);
+            Assert.AreEqual(Day.Saturday, day.Day);
+            Assert.AreEqual(8, day.Dates.Count);
+            Assert.AreEqual(8, day.Lessons.Count);
+
+            Assert.AreEqual(2, day.EarlierPossibleHour);
+            Assert.AreEqual(3, day.LastPossibleHour);
+            Assert.AreEqual(2, day.MaxPossibleCountLessons);
+        }
+
+        [TestMethod]
+        public void TestConstructorWitCountEducationWeekBySemProportionallyCountWeeksShedule()
+        {
+            SettingShedule setting = new SettingShedule(2, 6, 6, 6, 16, 4, 2, 1, 2, 4, 3);
+            day = getDay(setting = setting);
+            Assert.AreEqual(8, day.Dates.Count);
+        }
+
+        [TestMethod]
+        public void TestConstructorWitCountEducationWeekBySemDisproportionallyCountWeeksShedule()
+        {
+            SettingShedule setting = new SettingShedule(2, 6, 6, 6, 17, 4, 2, 1, 2, 4, 3);
+            day = getDay(setting = setting);
+            Assert.AreEqual(9, day.Dates.Count);
         }
 
         [TestMethod]
         public void TestFirstLessonHourInEmptyDay()
         {
-            Assert.AreEqual(day.FirstLessonHour, 0);
+            day = new SheduleDay();
+            Assert.AreEqual(0, day.FirstLessonHour);
         }
 
         [TestMethod]
         public void FirstLessonHour()
         {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -61,19 +111,20 @@ namespace TestShedule
 
             day.Lessons = lessons;
             
-            Assert.AreEqual(day.FirstLessonHour, 1);
+            Assert.AreEqual(1, day.FirstLessonHour);
         }
 
         [TestMethod]
         public void TestCountLessonsInEmptyDay()
         {
-            Assert.AreEqual(day.CountLessons, 0);
+            day = new SheduleDay();
+            Assert.AreEqual(0, day.CountLessons);
         }
 
         [TestMethod]
         public void TestCountLessons()
         {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -87,13 +138,13 @@ namespace TestShedule
 
             day.Lessons = lessons;
 
-            Assert.AreEqual(day.CountLessons, 2);
+            Assert.AreEqual(2, day.CountLessons);
         }
 
         [TestMethod]
         public void NonEmptyLessons()
         {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -107,17 +158,18 @@ namespace TestShedule
 
             day.Lessons = lessons;
 
-            Assert.AreEqual(day.NonEmptyLessons.Count(), 2);
+            Assert.AreEqual(2, day.NonEmptyLessons.Count());
         }
 
         [TestMethod]
         public void TestCountLessonsGroupInEmptyDay() {
-            Assert.AreEqual(day.CountLessonsGroup("ИВТ-260", Week.FirstWeek, Day.Monday), 0);
+            day = new SheduleDay();
+            Assert.AreEqual(0, day.CountLessonsGroup("ИВТ-260", Week.FirstWeek, Day.Monday));
         }
 
         [TestMethod]
         public void TestCountLessonsGroup() {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -130,19 +182,20 @@ namespace TestShedule
             };
             day.Lessons = lessons;
 
-            Assert.AreEqual(day.CountLessonsGroup("ИВТ-260", Week.FirstWeek, Day.Monday), 2);
-            Assert.AreEqual(day.CountLessonsGroup("ИВТ-262", Week.FirstWeek, Day.Monday), 0);
+            Assert.AreEqual(2, day.CountLessonsGroup("ИВТ-260", Week.FirstWeek, Day.Monday));
+            Assert.AreEqual(0, day.CountLessonsGroup("ИВТ-262", Week.FirstWeek, Day.Monday));
         }
 
         [TestMethod]
         public void TestLimitLessonsNotExceededInEmptyDay() {
-            Assert.AreEqual(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday), false);
+            day = new SheduleDay();
+            Assert.IsFalse(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday));
         }
 
         [TestMethod]
         public void TestLimitLessonsNotExceeded()
         {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -154,14 +207,14 @@ namespace TestShedule
             };
             day.Lessons = lessons;
 
-            Assert.AreEqual(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday), true);
+            Assert.IsTrue(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday));
         }
 
 
         [TestMethod]
         public void TestLimitLessons()
         {
-            day = getWeekDay();
+            day = getDay();
             SheduleTime time = new SheduleTime(Week.FirstWeek, Day.Monday, 1);
             List<DateTime> dates = new List<DateTime> { new DateTime(2016, 9, 6), new DateTime(2016, 9, 20) };
             List<string> groups = new List<string> { "ИВТ-260", "ИВТ-261" };
@@ -172,22 +225,23 @@ namespace TestShedule
                new SheduleLesson()
             };
             day.Lessons = lessons;
-            Assert.AreEqual(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday, 1), true);
-            Assert.AreEqual(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday, 28), false);
+            Assert.IsTrue(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday, 1));
+            Assert.IsFalse(day.LimitLessonsNotExceeded(new List<string> { "ИВТ-260" }, Week.FirstWeek, Day.Monday, 28));
 
         }
 
         [TestMethod]
         public void TestDatesDescriptionInEmptyDay()
         {
-            Assert.AreEqual(day.DatesDescription, "");
+            day = new SheduleDay();
+            Assert.AreEqual("", day.DatesDescription);
         }
 
         [TestMethod]
         public void TestDatesDescription()
         {
-            day = getWeekDay();
-            Assert.AreEqual(day.DatesDescription, "01.09, 15.09, 29.09");
+            day = getDay();
+            Assert.AreEqual("01.09, 15.09, 29.09, 13.10, 27.10, 10.11, 24.11, 08.12", day.DatesDescription);
         }
 
     }
